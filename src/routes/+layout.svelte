ae083c6910a0042cb6c6db4276f2f9a916d803df
鄭翊宏 <>
2023-03-20 11:30:13 +0800

    no message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script> import "../app.css"
 import Header from "../components/header.svelte";
 import Footer from "../components/footer.svelte";
+export const prerender = true;
   </script>
 
     <Header />
