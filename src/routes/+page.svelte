9d021943adb4bbe9227d763212298b0e2cd88c5e
鄭翊宏 <>
2023-03-20 13:46:05 +0800

    no message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,27 @@
 <script>
   import { onDestroy, onMount } from "svelte";
 
- let pic_src = '/assets/home_pic_one.jpeg'
-
-let toggle = ()=>{
-  pic_src = '/assets/home_pic_two.jpeg'
-}
-
-let picArray= ['/assets/home_pic_one.jpeg','/assets/home_pic_two.jpeg','/assets/home_pic_three.jpg'];
-$: picCurrent = '/assets/home_pic_one.jpeg';
-let picLoop = ()=>{
-  for(let i = 0 ;i<=picArray.length - 1;i++){
-      if(picCurrent == picArray[i]){
-        if((i+1) == picArray.length){
-          picCurrent = picArray[0]  
-        }else{
-          picCurrent = picArray[i+1];
-        }
-        break;
-      }
-    }
-}
-let LoopInterval
-onMount(async()=>{
-  LoopInterval = setInterval(picLoop,700)
-})
-onDestroy(async()=>{
-  clearInterval(LoopInterval)
-})
+// let picArray= ['https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/8cdcf1154323267.633fdd6b3efb6.jpg','/assets/home_pic_two.jpeg','/assets/home_pic_three.jpg'];
+$: picCurrent = 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/8cdcf1154323267.633fdd6b3efb6.jpg';
+// let picLoop = ()=>{
+//   for(let i = 0 ;i<=picArray.length - 1;i++){
+//       if(picCurrent == picArray[i]){
+//         if((i+1) == picArray.length){
+//           picCurrent = picArray[0]  
+//         }else{
+//           picCurrent = picArray[i+1];
+//         }
+//         break;
+//       }
+//     }
+// }
+// let LoopInterval
+// onMount(async()=>{
+//   LoopInterval = setInterval(picLoop,700)
+// })
+// onDestroy(async()=>{
+//   clearInterval(LoopInterval)
+// })
 </script>
 <h1 class=" font-bold text-[150px] text-center border-b border-white">YU-SHU</h1>
 <div class=" flex justify-between">
