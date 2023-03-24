2ee992d0083820bb29d9289d3385d34d6b609bf0
鄭翊宏 <>
2023-03-24 17:41:09 +0800

    0324

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,18 +30,27 @@
 <!-- <h1 class=" font-bold text-[100px] text-center border-b border-white">PHOTO</h1> -->
 <div class="w-full backImgreverse  rotate-180 z-10">
   <div class="w-full h-full py-10">
-    <div class="backImg w-1/2 mx-auto text-white h-[50vh] flex rotate-180">
+    
+    <div class="w-1/2 relative mx-auto text-white h-[50vh] flex rotate-180">
+   
       <div
         class="flex flex-col justify-between font-bold backdrop-blur-2xl w-full h-full rounded p-2"
       >
         <div>
+          <div class="backImg top-0 right-0 rounded-full absolute w-32 h-32">
+          </div>
           <span class=" text-[25px]">Hello!</span>
           <br />
           <span class=" text-[50px] text-[#A9A4A8]">I AM YUSHU</span>
           <br />
-          <span>LOCAL PHOTOGRAPHER.</span>
-        </div>
-        <div>
+          <span class="text-lg">LOCAL PHOTOGRAPHER.</span>
+          <br>
+          <div class="my-2">
+            <span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ab rerum natus assumenda, ratione animi amet quia aperiam, eum praesentium alias harum vero minus eaque adipisci recusandae nulla quae iure!</span>
+            <span > Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ab rerum natus assumenda, ratione animi amet quia aperiam, eum praesentium alias harum vero minus eaque adipisci recusandae nulla quae iure!</span>
+          </div>
+          </div>
+          <div>
           <span class="block text-end text-[12px]"
             >BASE IN TAOYUAN, 2023 , ALL RIGHT RESERVED</span
           >
@@ -58,8 +67,11 @@
     <button>people</button>
     <button>wedding</button>
   </div>
-  <div class="w-1/3 mx-auto overflow-hidden">
-    <img src={picCurrent1} alt="homepic" class="w-full h-full" />
+  <div class="w-1/3 mx-auto overflow-hidden flex flex-wrap">
+    <img src={picCurrent1} alt="homepic" />
+    <img src={picCurrent1} alt="homepic" />
+    <img src={picCurrent1} alt="homepic" />
+    <img src={picCurrent1} alt="homepic" />
   </div>
   <div
     class="flex flex-col w-full justify-center items-center gap-4 py-2 font-bold"
@@ -77,7 +89,7 @@
 <!-- https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/966731161277057.63df9745b4b39.jpg -->
 <style>
   .backImg {
-    background-image: url("");
+    background-image: url("https://mir-s3-cdn-cf.behance.net/project_modules/fs/8640bf154323267.633fdd6b3d3f5.jpg");
     background-position: center;
     background-size: cover;
     background-repeat: no-repeat;
